48a9edbc74012640bcccd9be73e505bed2143b81
DominikPeacock <der.dominik.peacock@gmail.com>
2020-05-26 10:09:05 +0200

    Fixed incorrectly generating {ModuleName}.cpp file

--- Plugins/ModuleGeneration/Resources/{ModuleName}/Private/{ModuleName}.cpp
+++ Plugins/ModuleGeneration/Resources/{ModuleName}/Private/{ModuleName}.cpp
@@ -1,42 +1,21 @@
-// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+// {Copyright}
 
 #include "ModuleGeneration.h"
-
 #include "Logging.h"
-#include "ModuleGenerationCommands.h"
-#include "NewModuleLogic.h"
 
 #include "Modules/ModuleManager.h"
-#include "ToolMenus.h"
 
-#define LOCTEXT_NAMESPACE "FModuleGenerationModule"
+#define LOCTEXT_NAMESPACE "F{ModuleName}"
 
-void FModuleGenerationModule::StartupModule()
+void F{ModuleName}::StartupModule()
 {
-	FModuleGenerationCommands::Register();
-
-	PluginCommands = MakeShareable(new FUICommandList);
-	PluginCommands->MapAction(
-		FModuleGenerationCommands::Get().NewModule,
-		FExecuteAction::CreateStatic(&NewModuleController::CreateAndShowNewModuleWindow),
-		FCanExecuteAction());
-	
-	UToolMenus::RegisterStartupCallback(FSimpleMulticastDelegate::FDelegate::CreateLambda(
-		[this]()
-		{
-			UToolMenu* FileMenu = UToolMenus::Get()->ExtendMenu("LevelEditor.MainMenu.File");
-			FToolMenuSection& Section = FileMenu->FindOrAddSection("FileProject");
-			Section.AddMenuEntryWithCommandList(FModuleGenerationCommands::Get().NewModule, PluginCommands);
-		}
-	));
 }
 
-void FModuleGenerationModule::ShutdownModule()
+void F{ModuleName}::ShutdownModule()
 {
-	// This function may be called during shutdown to clean up your module.  For modules that support dynamic reloading,
-	// we call this function before unloading the module.
+	
 }
 
 #undef LOCTEXT_NAMESPACE
 	
-IMPLEMENT_MODULE(FModuleGenerationModule, ModuleGeneration);
+IMPLEMENT_MODULE(F{ModuleName}, ModuleGeneration);
