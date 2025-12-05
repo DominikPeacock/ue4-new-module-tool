83bcb7d747b4192172393196932b39f7a3875372
Dominik Peacock <der.dominik.peacock@gmail.com>
2025-12-05 12:06:25 +0100

    Update to 5.7

--- Source/Plugin.Target.cs
+++ Source/Plugin.Target.cs
@@ -8,7 +8,7 @@ public class PluginTarget : TargetRules
 	public PluginTarget( TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Game;
-		DefaultBuildSettings = BuildSettingsVersion.V5;
+		DefaultBuildSettings = BuildSettingsVersion.Latest;
         IncludeOrderVersion = EngineIncludeOrderVersion.Latest;
 
         ExtraModuleNames.AddRange( new string[] { "Plugin" } );
--- Source/PluginEditor.Target.cs
+++ Source/PluginEditor.Target.cs
@@ -8,7 +8,7 @@ public class PluginEditorTarget : TargetRules
 	public PluginEditorTarget( TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Editor;
-		DefaultBuildSettings = BuildSettingsVersion.V5;
+		DefaultBuildSettings = BuildSettingsVersion.Latest;
 		IncludeOrderVersion = EngineIncludeOrderVersion.Latest;
 
 		ExtraModuleNames.AddRange( new string[] { "Plugin" } );
